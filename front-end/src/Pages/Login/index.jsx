4de926c017c29159c6dcb8b4a7aad41f9031b5af
pong-sun <johnpaul.banera@sun-asterisk.com>
2022-06-09 03:31:30 +0800

    Set up integration in login page

--- front-end/src/Pages/Login/index.jsx
+++ front-end/src/Pages/Login/index.jsx
@@ -1,20 +1,108 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './login.css';
 import logo from '../../Assets/Images/els.png';
 import * as Component from '../../Components';
+import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import { login } from '../../API/UserAPI';
+import { useDispatch } from 'react-redux';
+import _ from 'lodash';
+import { useCookies } from 'react-cookie';
+import { setUserToken, setUser } from '../../App/Redux/Slices/UserSlice';
 
 export default function Login() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [, setCookie] = useCookies();
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [flowState, setFlowState] = useState({
+    isLoading: false,
+    isError: { errors: { email: '', password: '' }, message: '' },
+  });
+  const { message } = flowState.isError;
+  const error = flowState.isError.errors ? flowState.isError.errors : {};
+
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    background: '#f5f5fc',
+    timer: 1500,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer);
+      toast.addEventListener('mouseleave', Swal.resumeTimer);
+    },
+  });
+
+  const onChange = (e) => {
+    setFormData((lastState) => ({
+      ...lastState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    setFlowState({ ...flowState, isLoading: true });
+
+    login(formData)
+      .then((res) => {
+        console.log(res);
+        setCookie('token', res.data.token);
+        setCookie('user', res.data.data);
+        dispatch(setUserToken(res.data.token));
+        dispatch(setUser(res.data.data));
+
+        Toast.fire({
+          icon: 'success',
+          title: 'Signed in successfully',
+        }).then(() => {
+          navigate('/dashboard');
+        });
+
+        setFormData({
+          email: '',
+          password: '',
+        });
+
+        setFlowState({ isLoading: false, isError: {} });
+      })
+      .catch((err) => {
+        setFlowState({ ...flowState, isError: err.response.data });
+      });
+  };
+
   return (
     <>
       <main className="login-body">
+        {flowState.isLoading && <Component.Spinner />}
         <img src={logo} alt="logo" />
         <div className="left-container"></div>
         <div className="right-container">
           <h1>Welcome Back</h1>
-          <form action="#">
+          <form onSubmit={onSubmit}>
+            <span className="error-field" style={{ textAlign: 'center' }}>
+              {_.size(error) === 0 ? message : null}
+            </span>
             <div className="email">
               <label htmlFor="email">Email</label>
-              <input type="text" name="email" placeholder="john.doe@mail.com" />
+              <input
+                type="text"
+                name="email"
+                placeholder="john.doe@mail.com"
+                onChange={onChange}
+                className={error.email ? 'input-error' : ''}
+              />
+              <span className="error-field">
+                {error.email ? error.email : null}
+              </span>
             </div>
             <div className="password">
               <label htmlFor="password">Password</label>
@@ -22,7 +110,12 @@ export default function Login() {
                 type="password"
                 name="password"
                 placeholder="- - - - - - - -"
+                onChange={onChange}
+                className={error.password ? 'input-error' : ''}
               />
+              <span className="error-field">
+                {error.password ? error.password : null}
+              </span>
             </div>
             <div className="remember">
               <input type="checkbox" name="remember" id="remember" />
